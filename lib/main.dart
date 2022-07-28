3a5d9811001a54dfb81e9db19e130beb326bb6e9
Waqad <waqadarshad2@gmail.com>
2022-07-28 20:17:01 +0500

    Took 44 minutes completed firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:provider/provider.dart';
 import 'package:vip_picnic/config/routes/routes_config.dart';
 import 'package:vip_picnic/config/theme/light_theme.dart';
+import 'package:vip_picnic/firebase_options.dart';
 import 'package:vip_picnic/provider/chat_provider/chat_provider.dart';
 import 'package:vip_picnic/provider/story_provider/story_provider.dart';
 import 'package:vip_picnic/utils/localization.dart';
@@ -10,25 +13,70 @@ import 'package:vip_picnic/utils/localization.dart';
 import 'provider/chat_provider/chat_head_provider.dart';
 import 'provider/user_provider/user_provider.dart';
 
-void main() => runApp(
-      MultiProvider(
-        providers: [
-          ChangeNotifierProvider(
-            create: (_) => UserProvider(),
-          ),
-          ChangeNotifierProvider(
-            create: (_) => ChatHeadProvider(),
-          ),
-          ChangeNotifierProvider(
-            create: (_) => ChatProvider(),
-          ),
-          ChangeNotifierProvider(
-            create: (_) => StoryProvider(),
-          ),
-        ],
-        child: MyApp(),
-      ),
-    );
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+
+  print("Handling a background message: ${message.messageId}");
+}
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+
+    if (message.notification != null) {
+      print('Message also contained a notification: ${message.notification}');
+    }
+  });
+
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+  if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+    print("This simulator is authorized to accept android notification.");
+  }
+
+  debugPrint('User granted permission: ${settings.authorizationStatus}');
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => UserProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => ChatHeadProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => ChatProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => StoryProvider(),
+        ),
+      ],
+      child: MyApp(),
+    ),
+  );
+}
 
 class MyApp extends StatelessWidget {
   @override
